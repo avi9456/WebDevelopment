01274b5eda4774acab0f8e2e6500a1f69522397e
Avinash Kumar Gupta <avinashkumargupta@Avinashs-MacBook-Pro.local>
2024-06-20 23:40:08 +0530

    day 8

---
+++ BackEnd/8.3 Travel Tracker/index.js
@@ -0,0 +1,76 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+  user:"avinashkumargupta",
+  password:"",
+  host:"localhost",
+  port:5432,
+  database:"world"
+});
+db.connect();
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+async function checkVisited(){
+  const result = await db.query("select country_code from visited_countries");
+  let countries = [];
+  result.rows.forEach((country)=>{
+    countries.push(country.country_code);
+  });
+  return countries;
+}
+
+app.get("/", async (req, res) => {
+  //Write your code here.
+  
+  let countries = await checkVisited();
+  res.render("index.ejs",{
+    total:  countries.length,
+    countries: countries,
+  });
+});
+
+app.post("/add",async (req,res)=>{
+  const country = req.body.country;
+  
+  try{
+
+    const result = await db.query("select country_code from countries where LOWER(country_name) LIKE '%' || $1 || '%'",
+      [country.toLowerCase()]);
+
+    console.log(result.rows);
+    const country_code = result.rows[0].country_code;
+    console.log(country_code);
+
+    try{
+      await db.query("insert into visited_countries (country_code) values ($1)",[country_code,]);
+      res.redirect("/");
+    }catch(err){
+      console.log(err);
+      let countries = await checkVisited();
+      res.render("index.ejs",{
+        total: countries.length,
+        countries: countries,
+        error: "Country has already been added, try again."
+      });
+    }
+  }catch(err){
+    console.log(err);
+    let countries = await checkVisited();
+    res.render("index.ejs",{
+      total: countries.total,
+      countries: countries,
+      error: "Country name does not exist, try again."
+    });
+  }
+});
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
